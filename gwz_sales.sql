1610b8ccbc9fed938fcb44063dae633a8bf49bf9
Gulruh Duruk <baslangc@gmail.com>
2026-02-27 14:07:10 +0300

    Update gwz_sales.sql
    Round results to 2 decimal places

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,7 +1,7 @@
 SELECT
   date_date,
   ROUND(SUM(turnover), 2) AS daily_turnover,
-  ROUND(SUM(purchase_cost), 2) AS daily_purchase_cost -- Bu yeni eklediğimiz satır
+  ROUND(SUM(purchase_cost), 2) AS daily_purchase_cost
 FROM `course14.gwz_sales`
 GROUP BY
   date_date
